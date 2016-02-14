d8aec4557cc6d9e673ee9776d795912f9f184872
macro <macro_fengye@163.com>
2016-02-14 16:09:42 +0800

    去掉冗余的代码

---
+++ socketman/test.zep
@@ -0,0 +1,12 @@
+namespace Socketman;
+
+class Test{
+	public function say(string !name="jack"){
+		let name = " kkkkkppppp";
+		var a1 , a2;
+		let a1 = null;
+		let a2 = null;
+		explode("\n" , name);
+		echo "Hello" . name;
+	}
+}
--- tests/test.php
+++ tests/test.php
@@ -1,7 +1,7 @@
 <?php
 Socketman\Greeting::say() . "\n";
-echo Socketman\Greeting::fibonacci(2) . "\n";
 echo Socketman\Greeting::sum(20, 80) . "\n";
 echo Socketman\Greeting::start(array("ip" => "127.0.0.1", 'port' => 9090)) . "\n";
-
+$test = new Socketman\Test();
+$test->say(" kkkk") . "\n";
 sleep(10);
